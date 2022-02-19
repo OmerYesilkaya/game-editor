2ad8ddef67be48b29f6c2fb5bb387f0b5590b22e
OmerYesilkaya <omerfarukyesilkaya@gmail.com>
2022-02-19 15:52:16 +0300

    fix format camel case function reges

--- core/utils/stringUtils.ts
+++ core/utils/stringUtils.ts
@@ -1,12 +1,12 @@
 function capitalize(value: string) {
-	return value
-		.split(" ")
-		.map((word) => `${word[0].toUpperCase()}${word.slice(1, word.length)}`)
-		.join(" ");
+    return value
+        .split(" ")
+        .map((word) => `${word[0].toUpperCase()}${word.slice(1, word.length)}`)
+        .join(" ");
 }
 
 function formatCamelCase(value: string) {
-	return value.split(/(?=[A-Z])/).join(" ");
+    return value.split(/(?<=[a-z])(?=[A-Z])/).join(" ");
 }
 
 export default { capitalize, formatCamelCase };
